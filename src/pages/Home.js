80ee6699f514ad3e5913d872e074c41ea60afd6f
unknown <tgantara@gmail.com>
2022-02-25 15:57:34 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,63 @@
-import React from 'react'
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import Card from '../components/Card';
 import CardProggress from '../components/CardProggress';
-
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule'
 
 function Home() {
+    let dataAgenda = new Object();
+    let _dataAgenda = [];
+    const [timeSalat, setTimeSalat] = useState([]);
+    const [agenda, setAgenda] = useState(null);
+    const [data, setData] = useState([
+        {
+            id: "1",
+            perihal: "Bimbingan Teknis 1",
+            lokasi: "Bandung",
+            tanggal_mulai: "2022-02-01",
+            tanggal_selesai: "2022-02-02",
+            jam_mulai: "08:30:00",
+            jam_selesai: "12:00:00",
+        },
+        {
+            id: "2",
+            perihal: "Bimbingan Teknis Lanjutan",
+            lokasi: "Bandung",
+            tanggal_mulai: "2022-02-10",
+            tanggal_selesai: "2022-02-11",
+            jam_mulai: "08:30:00",
+            jam_selesai: "12:00:00",
+        },
+        {
+            id: "3",
+            perihal: "Bimbingan Teknis Lanjutan 3",
+            lokasi: "Bandung",
+            tanggal_mulai: "2022-02-13",
+            tanggal_selesai: "2022-02-14",
+            jam_mulai: "08:30:00",
+            jam_selesai: "12:00:00",
+        }
+    ]);
+
+
+    useEffect(() => {
+        Axios.get(`https://api.pray.zone/v2/times/today.json?city=bandung`).then(res => {
+            let salat = res.data.results.datetime[0].times
+            setTimeSalat(salat)
+        })
+        for (let i = 0; i < data.length; i++) {
+            _dataAgenda.push({ Id: data[i].id, Subject: data[i].perihal, StartTime: data[i].tanggal_mulai + ' ' + data[i].jam_mulai, EndTime: data[i].tanggal_selesai + ' ' + data[i].jam_selesai })
+        }
+        setAgenda(_dataAgenda)
+        console.log(_dataAgenda)
+    }, [])
     return (
         <div className="container-fluid">
             {/* Page Heading */}
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
                 <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i className="fas fa-download fa-sm text-white-50" /> Generate
-                    Report</a>
+                </a>
             </div>
             {/* Content Row */}
             <div className="row">
@@ -26,12 +73,12 @@ function Home() {
             {/* Content Row */}
             <div className="row">
                 {/* Area Chart */}
-                <div className="col-xl-8 col-lg-7">
+                <div className="col-xl-4 col-lg-6">
                     <div className="card shadow mb-4">
                         {/* Card Header - Dropdown */}
                         <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                             <h6 className="m-0 font-weight-bold text-primary">
-                                Earnings Overview
+                                Waktu Shalat Hari Ini
                             </h6>
                             <div className="dropdown no-arrow">
                                 <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -48,19 +95,40 @@ function Home() {
                         </div>
                         {/* Card Body */}
                         <div className="card-body">
-                            <div className="chart-area">
-                                <canvas id="myAreaChart" />
-                            </div>
+                            <table className="table table-striped table-responsive">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Imsak</th>
+                                        <th scope="col">Subuh</th>
+                                        <th scope="col">Terbit</th>
+                                        <th scope="col">Duhur</th>
+                                        <th scope="col">Asar</th>
+                                        <th scope="col">Magrib</th>
+                                        <th scope="col">Isya</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{timeSalat.Imsak}</td>
+                                        <td>{timeSalat.Fajr}</td>
+                                        <td>{timeSalat.Sunrise}</td>
+                                        <td>{timeSalat.Dhuhr}</td>
+                                        <td>{timeSalat.Asr}</td>
+                                        <td>{timeSalat.Maghrib}</td>
+                                        <td>{timeSalat.Isha}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
                 {/* Pie Chart */}
-                <div className="col-xl-4 col-lg-5">
+                <div className="col-xl-8 col-lg-8">
                     <div className="card shadow mb-4">
                         {/* Card Header - Dropdown */}
                         <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                             <h6 className="m-0 font-weight-bold text-primary">
-                                Revenue Sources
+                                Agenda
                             </h6>
                             <div className="dropdown no-arrow">
                                 <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -77,174 +145,12 @@ function Home() {
                         </div>
                         {/* Card Body */}
                         <div className="card-body">
-                            <div className="chart-pie pt-4 pb-2">
-                                <canvas id="myPieChart" />
-                            </div>
-                            <div className="mt-4 text-center small">
-                                <span className="mr-2">
-                                    <i className="fas fa-circle text-primary" /> Direct
-                                </span>
-                                <span className="mr-2">
-                                    <i className="fas fa-circle text-success" /> Social
-                                </span>
-                                <span className="mr-2">
-                                    <i className="fas fa-circle text-info" /> Referral
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* Content Row */}
-            <div className="row">
-                {/* Content Column */}
-                <div className="col-lg-6 mb-4">
-                    {/* Project Card Example */}
-                    <div className="card shadow mb-4">
-                        <div className="card-header py-3">
-                            <h6 className="m-0 font-weight-bold text-primary">Projects</h6>
-                        </div>
-                        <div className="card-body">
-                            <h4 className="small font-weight-bold">
-                                Server Migration <span className="float-right">20%</span>
-                            </h4>
-                            <div className="progress mb-4">
-                                <div className="progress-bar bg-danger" role="progressbar" style={{ width: '20%' }} aria-valuenow={20} aria-valuemin={0} aria-valuemax={100} />
-                            </div>
-                            <h4 className="small font-weight-bold">
-                                Sales Tracking <span className="float-right">40%</span>
-                            </h4>
-                            <div className="progress mb-4">
-                                <div className="progress-bar bg-warning" role="progressbar" style={{ width: '40%' }} aria-valuenow={40} aria-valuemin={0} aria-valuemax={100} />
-                            </div>
-                            <h4 className="small font-weight-bold">
-                                Customer Database <span className="float-right">60%</span>
-                            </h4>
-                            <div className="progress mb-4">
-                                <div className="progress-bar" role="progressbar" style={{ width: '60%' }} aria-valuenow={60} aria-valuemin={0} aria-valuemax={100} />
-                            </div>
-                            <h4 className="small font-weight-bold">
-                                Payout Details <span className="float-right">80%</span>
-                            </h4>
-                            <div className="progress mb-4">
-                                <div className="progress-bar bg-info" role="progressbar" style={{ width: '80%' }} aria-valuenow={80} aria-valuemin={0} aria-valuemax={100} />
-                            </div>
-                            <h4 className="small font-weight-bold">
-                                Account Setup <span className="float-right">Complete!</span>
-                            </h4>
-                            <div className="progress">
-                                <div className="progress-bar bg-success" role="progressbar" style={{ width: '100%' }} aria-valuenow={100} aria-valuemin={0} aria-valuemax={100} />
-                            </div>
-                        </div>
-                    </div>
-                    {/* Color System */}
-                    <div className="row">
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-primary text-white shadow">
-                                <div className="card-body">
-                                    Primary
-                                    <div className="text-white-50 small">#4e73df</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-success text-white shadow">
-                                <div className="card-body">
-                                    Success
-                                    <div className="text-white-50 small">#1cc88a</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-info text-white shadow">
-                                <div className="card-body">
-                                    Info
-                                    <div className="text-white-50 small">#36b9cc</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-warning text-white shadow">
-                                <div className="card-body">
-                                    Warning
-                                    <div className="text-white-50 small">#f6c23e</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-danger text-white shadow">
-                                <div className="card-body">
-                                    Danger
-                                    <div className="text-white-50 small">#e74a3b</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-secondary text-white shadow">
-                                <div className="card-body">
-                                    Secondary
-                                    <div className="text-white-50 small">#858796</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-light text-black shadow">
-                                <div className="card-body">
-                                    Light
-                                    <div className="text-black-50 small">#f8f9fc</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Dark
-                                    <div className="text-white-50 small">#5a5c69</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-6 mb-4">
-                    {/* Illustrations */}
-                    <div className="card shadow mb-4">
-                        <div className="card-header py-3">
-                            <h6 className="m-0 font-weight-bold text-primary">
-                                Illustrations
-                            </h6>
-                        </div>
-                        <div className="card-body">
-                            <div className="text-center">
-                                <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: '25rem' }} src="img/undraw_posting_photo.svg" alt="..." />
-                            </div>
-                            <p>
-                                Add some quality, svg illustrations to your project
-                                courtesy of
-                                <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images
-                                that you can use completely free and without attribution!
-                            </p>
-                            <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw →</a>
-                        </div>
-                    </div>
-                    {/* Approach */}
-                    <div className="card shadow mb-4">
-                        <div className="card-header py-3">
-                            <h6 className="m-0 font-weight-bold text-primary">
-                                Development Approach
-                            </h6>
-                        </div>
-                        <div className="card-body">
-                            <p>
-                                SB Admin 2 makes extensive use of Bootstrap 4 utility
-                                classes in order to reduce CSS bloat and poor page
-                                performance. Custom CSS classes are used to create custom
-                                components and custom utility classes.
-                            </p>
-                            <p className="mb-0">
-                                Before working with this theme, you should become familiar
-                                with the Bootstrap framework, especially the utility
-                                classes.
-                            </p>
+                            <ScheduleComponent currentView='Month'
+                                eventSettings={{
+                                    dataSource: agenda
+                                }}>
+                                <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+                            </ScheduleComponent>
                         </div>
                     </div>
                 </div>
